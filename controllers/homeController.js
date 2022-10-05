a9e652403748bdebe289555874b539e8c46f34c7
Ayush <aayush199813@gmail.com>
2022-10-05 17:31:37 +0530

    Bug of search

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,61 +1,6 @@
 const File = require('../models/file');
 
 exports.getHome = (req, res, next) => {
-  // const file = new File({
-  //   fileName: "csv2",
-  //   data: [
-  //     {
-  //       1: '2',
-  //       3: '293',
-  //       35: '68.02',
-  //       'Eldon Base for stackable storage shelf, platinum':
-  //         '1.7 Cubic Foot Compact "Cube" Office Refrigerators',
-  //       'Muhammed MacIntyre': 'Barry French',
-  //       '-213.25': '457.81',
-  //       38.94: '208.16',
-  //       Nunavut: 'Nunavut',
-  //       'Storage & Organization': 'Appliances',
-  //       0.8: '0.58',
-  //     },
-  //     {
-  //       1: '3',
-  //       3: '293',
-  //       35: '2.99',
-  //       'Eldon Base for stackable storage shelf, platinum':
-  //         'Cardinal Slant-D� Ring Binder, Heavy Gauge Vinyl',
-  //       'Muhammed MacIntyre': 'Barry French',
-  //       '-213.25': '46.71',
-  //       38.94: '8.69',
-  //       Nunavut: 'Nunavut',
-  //       'Storage & Organization': 'Binders and Binder Accessories',
-  //       0.8: '0.39',
-  //     },
-  //     {
-  //       1: '4',
-  //       3: '483',
-  //       35: '3.99',
-  //       'Eldon Base for stackable storage shelf, platinum': 'R380',
-  //       'Muhammed MacIntyre': 'Clay Rozendal',
-  //       '-213.25': '1198.97',
-  //       38.94: '195.99',
-  //       Nunavut: 'Nunavut',
-  //       'Storage & Organization': 'Telephones and Communication',
-  //       0.8: '0.58',
-  //     },
-  //   ],
-  // });
-
-  // file
-  //   .save()
-  //   .then(() => {
-  //     res.render('index', {
-  //       pageTitle: 'Home',
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-
   File.find()
     .then((files) => {
       res.render('index', {
@@ -88,17 +33,33 @@ exports.postFile = (req, res, next) => {
   const search = req.body.search;
   File.findById(fileId)
     .then((file) => {
-      const key = Object.keys(file.data[0]);
-      const appendedData = file.data.filter((res, index, arr) => {
-        arr.push('new');
-        return res.Number == search || res.Footnote == search;
+      const keys = Object.keys(file.data[0]);
+      const appendedData = file.data.filter((res) => {
+        return (
+          res[keys[0]] == search ||
+          res[keys[1]] == search ||
+          res[keys[2]] == search ||
+          res[keys[3]] == search ||
+          res[keys[4]] == search ||
+          res[keys[5]] == search ||
+          res[keys[6]] == search
+        );
+        // let isValid = true;
+        // for (let i = 0; i < keys.length; i++) {
+        //   console.log(res[keys[i]] == search);
+        //   // if (res[keys[i]] != search) {
+        //   //   isValid = false;
+        //   // };
+        //   isValid=isValid && res[keys[i]==search]
+        // }
+        // return isValid;
       });
       res.render('table', {
         pageTitle: 'Tabluar',
         file: file,
         fileData: appendedData,
         length: Object.keys(file.data[0]).length,
-        tableHeading: key,
+        tableHeading: keys,
       });
     })
     .catch((err) => console.log(err));
